35d3d43635ee9015e8900318d44da5fd74ef62de
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-02-03 18:28:49 +0530

    created header

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './Header/Header';
 import Navbar from './Navbar/Navbar';
 
 function Layout({ children }) {
     return (
         <>
             <Navbar />
+            <Header />
             {children}
         </>
     );
